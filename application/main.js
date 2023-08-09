7d2a30b8d62e6454b7effc8ff84974c315261714
michaelzarubin24 <zarubinmihail99@gmail.com>
2023-08-09 10:55:44 +0300

    done

--- application/main.js
+++ application/main.js
@@ -1,5 +1,17 @@
 "use strict";
-alert("Welcome!");
+// ----------------------------------------------------------------
+const hiddenNemo = "fvgybuhnemofgbhnj";
+function findNemo(text) {
+  const index = text.indexOf("nemo");
+  if (index !== -1) {
+    return `Nemo found at index ${index}.`;
+  } else {
+    return "Nemo not found.";
+  }
+}
+
+console.log(findNemo(hiddenNemo));
+// ----------------------------------------------------------------
 const nemo = {
   firstName: {
     surname: {
@@ -11,3 +23,12 @@ const nemo = {
 };
 
 console.log(nemo.firstName.surname.text.msg);
+// ----------------------------------------------------------------
+
+const arr = ["not nemo", "not nemo", "nemo", "not nemo", "not nemo", "nemo"];
+
+for (let i = 0; i < arr.length; i++) {
+  const nemo = arr[i];
+  if (nemo === "nemo") console.log(nemo);
+}
+// ---------------------------------------------------------------
