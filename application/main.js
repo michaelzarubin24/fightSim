94234f249845c4fa3bf03fad274f5680a2af76ba
michaelzarubin24 <zarubinmihail99@gmail.com>
2023-08-14 19:03:54 +0300

    done

--- application/main.js
+++ application/main.js
@@ -1,44 +1,100 @@
 "use strict";
 // ----------------------------------------------------------------
-function main() {
-  const number1 = getNumber();
-  const number2 = getNumber();
-  const symbol = prompt("Please enter symbol (+ - * /)");
+alert("Welcome!");
+let isArray = Array.isArray;
+const extractData = (people, tag) => {
+  if (isArray(people)) {
+    let sum = 0;
+    for (const person of people) {
+      const value = person[tag];
+      sum += value;
+    }
+    console.log(sum);
+    return sum;
+  }
+};
+const BUDGET_BY_DEFAULT = 20000;
 
-  switch (symbol) {
-    case "+":
-      alert(number1 + number2);
+const IT_STEP_ACADEMY = {
+  budget: BUDGET_BY_DEFAULT,
+  teachers: [
+    {
+      id: "000001",
+      name: "Albert",
+      surname: "Einstein",
+      income: 3000,
+    },
+    {
+      id: "000002",
+      name: "Nicola",
+      surname: "Tesla",
+      income: 4000,
+    },
+    {
+      id: "000003",
+      name: "Steve",
+      surname: "Jobs",
+      income: 2000,
+    },
+    {
+      id: "000004",
+      name: "Robert",
+      surname: "Oppenheimer",
+      income: 8000,
+    },
+  ],
+
+  students: [
+    {
+      id: "100001",
+      name: "Michael",
+      surname: "Zarubin",
+      outcome: 3000,
+    },
+    {
+      id: "100002",
+      name: "Vlad",
+      surname: "Pitt",
+      outcome: 2000,
+    },
+    {
+      id: "100003",
+      name: "Bread",
+      surname: "Hleb",
+      outcome: 4000,
+    },
+    {
+      id: "100004",
+      name: "Harry",
+      surname: "Potter",
+      outcome: 5000,
+    },
+  ],
+};
+
+for (const key in IT_STEP_ACADEMY) {
+  const value = IT_STEP_ACADEMY[key];
+
+  switch (key) {
+    case "students":
+      extractData(value, "outcome");
       break;
-    case "-":
-      alert(number1 - number2);
+    case "teachers":
+      extractData(value, "income");
       break;
-    case "*":
-      alert(number1 * number2);
-      break;
-    case "/":
-      alert(number1 / number2);
+
+    case "budget":
+      const outcome = extractData(IT_STEP_ACADEMY.students, "outcome");
+      const income = extractData(IT_STEP_ACADEMY.teachers, "income");
+      const prev_budget = IT_STEP_ACADEMY[key];
+
+      IT_STEP_ACADEMY.budget = outcome - income;
+      console.log(outcome - income);
+      console.log("[prev_budget]", prev_budget);
+      console.log("[actual_budget]", IT_STEP_ACADEMY.budget);
       break;
 
     default:
-      alert("Error, you are 404");
       break;
   }
-
-  function isInvalid(number) {
-    return isNaN(number);
-  }
-  function getNumber() {
-    let isRunning = true;
-
-    while (isRunning) {
-      const number = +prompt("Enter number!");
-      isRunning = isInvalid(number);
-      if (!isRunning) return number;
-      if (isRunning) {
-        alert("Please enter numbers");
-      }
-    }
-  }
 }
-
-main();
